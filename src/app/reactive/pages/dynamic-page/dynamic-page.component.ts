3c985093bdaef78548ffeeeacdaf21ba0ba61eae
felipe alferez <alferezfelipe@gmail.com>
2024-02-02 19:11:57 -0500

    add form array

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
+
+type ValidFields = 'name' | 'favoriteGames'
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
 })
 export class DynamicPageComponent {
+  constructor(private fb: FormBuilder) { }
 
+  form = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    ],
+    favoriteGames: this.fb.array([
+      ['COD', Validators.required],
+      ['FIFA', Validators.required],
+    ])
+  })
+
+  get favoriteGames() {
+    return this.form.controls['favoriteGames'] as FormArray
+  }
+
+  triggerErrors(field: ValidFields) : boolean | null {
+    return this.form.controls[field].errors && this.form.controls[field].touched
+  }
+
+  isValidFieldInArray(form : FormArray, index : number) {
+    return form.controls[index].errors && form.controls[index].touched
+  }
+
+  getFieldErrors(field: ValidFields): string | null {
+    if (!this.form.controls[field]) return null;
+    const errors = this.form.controls[field].errors || {}
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return `Este campo requiere minimo ${errors['minlength'].requiredLength} caracteres`
+      }
+    }
+    return null
+  }
+  onSubmit() { 
+    if(this.form.invalid) return
+    console.log('Sending...')
+  }
 }
